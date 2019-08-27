d9405e5cc60a0fa519b5747311f85df69374a357
Dhendy <dhendykur2@gmail.com>
2019-08-27 20:19:39 +0700

    a

--- src/models/pokemon/fetchPokemon.js
+++ src/models/pokemon/fetchPokemon.js
@@ -2,14 +2,14 @@ const axios = require('axios');
 
 const main = async (_, args) => {
   const { limit, offset } = args;
-  const res = await axios.default.get(`https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}'`);
-  const data = [];
-  for (let i = 0; i< res.data.results.length; i++) {
-    const res1 = await axios.default.get(`https://pokeapi.co/api/v2/pokemon/${res.data.results[i].name}`);
-    data.push({ ...res1.data, url: res.data.results[i].url })
-  }
+  const res = await axios.default.get(`https://pokeapi.co/api/v2/pokemon/`, {
+    params: {
+      offset,
+      limit
+    }
+  });
   return {
-    pokemons: data
+    pokemons: res.data.results
   };
 };
 
